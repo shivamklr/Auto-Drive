39298bc1833ef881386e21dd5ff605cfcbab1c4f
shivamklr <shivamkalra98@gmail.com>
2018-11-23 20:18:35 +0530

    Implemented Sigmoid using Keras

--- keras-prediction/perceptron1.py
+++ keras-prediction/perceptron1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import keras 
 from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 
 n_pts = 500
@@ -16,4 +18,9 @@ y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
 plt.scatter(X[:n_pts,0], X[:n_pts,1])
 plt.scatter(X[n_pts:,0], X[n_pts:,1])
 plt.show()
+
 model = Sequential()
+model.add(Dense(units=1, input_shape = (2,), activation = 'sigmoid'))
+adam = Adam(lr = 0.1)
+model.compile(adam, loss='binary_crossentropy', metrics= ['accuracy'])
+model.fit(x=X, y=y, verbose=1, batch_size = 50, epochs = 500, shuffle ='true')
